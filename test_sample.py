c18905af14fd2d8515181c939caae8fe400c1939
Alexander <alexander.castro01@uptc.edu.co>
2022-02-01 18:54:39 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,8 +1,8 @@
 """Testing."""
 def inc(age_user):
     """Method #01"""
-    return age_user + 1
+    return age_user - 1
 
 def test_answer():
     """Test #01"""
-    assert inc(3) == 4
+    assert inc(18) == 19
